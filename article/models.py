8651ad44586ae67f98a9c644cb671d8a256d6932
kihoongit <kitto5537@gmail.com>
2022-10-28 16:50:04 +0900

    Add : Create Model : User,Article

--- article/models.py
+++ article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    pass
+
+class Article(models.Model):
+    author = models.Foreignkey(User, on_delete = models.CASCADE)
+    title = models.Charfield()
+    content = models.Textfield()
