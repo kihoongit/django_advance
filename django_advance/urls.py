2dec9947b90a77f926533fcced9d66b2ca22b64d
kihoongit <kitto5537@gmail.com>
2022-10-28 16:39:42 +0900

    Add : Create App. article

--- django_advance/urls.py
+++ django_advance/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
